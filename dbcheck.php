deec899fe909a237db5545f3ba5f92170a48df78
Jeong su <gamdong91@naver.com>
2017-06-01 20:25:27 +0900

    회원가입 완료

---
+++ dbcheck.php
@@ -0,0 +1,21 @@
+	<?php
+	/*
+		우선 디비 체크
+	*/
+	$DB_ADDR ="localhost";
+	$DB_USER = "root";
+	$DB_PW = "1234";
+	$DB_NAME = "webser";
+
+	header("charset=UTF-8");
+
+	$db=mysqli_connect($DB_ADDR,$DB_USER,$DB_PW,$DB_NAME);
+
+	if($db){
+		
+	}
+	else{
+		echo "DB Connection failed";
+	}
+
+	?>
--- signuppage.php
+++ signuppage.php
@@ -38,61 +38,42 @@
 
         function dodo(){
             console.log("click success!");
-/*
-                                    var eid=$("#idInput").val();
-                                    var password=$("#passwordInInput").val();
-                                    var name=$("#nameInput").val();
-                                    var add=$("#numInput").val();
-                                    var address=$("#addInput").val();
-                                    var phone=$("#phoneInput").val();
-                                    var email=$("#emailInput").val();
-                                    console.log(eid);
-                                    console.log(password);
-                                    console.log(name);
-                                    console.log(add);
-                                    console.log(address);
-                                    console.log(phone);
-                                    console.log(email);
-                                    */
-                                    
 
-                                    $.ajax({
-                                        type:"POST",
-                                        url:"./signupcheck.php",
-                                        data:{},
-                                        dataType:"text",
-                                        success:function(rtn){
-                                            if(rtn=="signUpSuccess"){
-                                                console.log(rtn);
-                                                alert("회원가입 성공 ");
-                                                document.location.href="./index.php";
-                                                }
-                                            else{
-                                                console.log(rtn);
-                                                alert("회원가입에 실패하였습니다");
-                                            }
-                                        },
-                                        error:function(e){
-                                        }
-                                    });
+            var phone=$("#inputphone").val();
+            var password=$("#inputpw").val();
+            var name=$("#inputname").val();
+            var email=$("#inputEmail").val();
+            var biz=$("#biz").val();
+
+            console.log(phone);
+            console.log(password);
+            console.log(name);
+            console.log(email);
+            console.log(biz);
+
+
+            $.ajax({
+                type:"POST",
+                url:"./signupcheck.php",
+                data:{phone:phone, password:password, name:name, email:email,biz:biz},
+                dataType:"text",
+                success:function(rtn){
+                alert("회원가입 성공하셨습니다.");
+                document.location.href="./index.php";
+
+                },
+                error:function(e){
+                }
+            });
 
         }
     </script>
     </head>
     <body class="">
       <!-- Pushy Menu -->
-      <nav class="pushy pushy-left">
-        <ul class="list-unstyled">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#feat">Features</a></li>
-            <li><a href="#about">About me</a></li>
-            <li><a href="#news">My Blog</a></li>
-            <li><a href="#history">My History</a></li>
-            <li><a href="#photos">Look my Photos</a></li>
-            <li><a href="#contact">Get in Touch!</a></li>
-            <li><a href="http://www.themeinthebox.com/ourtheme/mountain-king-bootstrap-template/" target="_blank">Download</a></li>
-        </ul>
-      </nav>
+    <?php
+    include("nav.php");
+    ?>
 
       <!-- Site Overlay -->
       <div class="site-overlay"></div>
@@ -114,14 +95,19 @@
                     <div class="jumbotron">
  
 
-                        <!-- 나중에 id 값 다 바꾸기 일단 폼만 해놓음 -->
+                        
                         <form class="form-horizontal">
 
-
-                          <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">아이디</label>
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">이메일</label>
                             <div class="col-sm-10">
-                              <input type="email" class="form-control" id="inputEmail3" placeholder="아이디">
+                              <input type="email" class="form-control" id="inputEmail" placeholder="이메일">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">전화번호</label>
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control" id="inputphone" placeholder="번화번호">
                             </div>
                           </div>
 
@@ -129,7 +115,7 @@
                           <div class="form-group">
                             <label for="inputPassword3" class="col-sm-2 control-label">비밀번호</label>
                             <div class="col-sm-10">
-                              <input type="password" class="form-control" id="inputPassword3" placeholder="비밀번호">
+                              <input type="password" class="form-control" id="inputpw" placeholder="비밀번호">
                             </div>
                           </div>
                         
@@ -137,25 +123,17 @@
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-label">이름</label>
                             <div class="col-sm-10">
-                              <input type="email" class="form-control" id="inputEmail3" placeholder="이름">
+                              <input type="email" class="form-control" id="inputname" placeholder="이름">
                             </div>
                         </div>
 
+                      
+
                         <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">이메일</label>
-                            <div class="col-sm-10">
-                              <input type="email" class="form-control" id="inputEmail3" placeholder="이메일">
-                            </div>
-                          </div>
-
-
-                        <div class="checkbox">
-                          <label>
-
-
-                            <input type="checkbox" value="" style="float:none;">
-                            사업자일경우 체크 해주십시오.
-                          </label>
+                             <select name="biz" id="biz">
+                            <option value="1">사업자</option>
+                            <option value="2">사용자</option>
+                            </select>
                         </div>
 
                         </form>
@@ -169,195 +147,12 @@
                 </div>
             </div>
         </header>
-        <section id="feat">
-            <div class="container">
-                <div class="row features">
-                    <div class="col-md-4 text-center wow fadeInUp" data-wow-delay="100ms">
-                        <span class="typcn typcn-pencil x3"></span>
-                        <h4>Consectetur Risus</h4>
-                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    </div>
-                    <div class="col-md-4 text-center wow fadeInUp" data-wow-delay="300ms">
-                        <span class="typcn typcn-camera-outline x3"></span>
-                        <h4>Ultricies Aenean</h4>
-                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                    </div>
-                    <div class="col-md-4 text-center wow fadeInUp" data-wow-delay="500ms">
-                        <span class="typcn typcn-bookmark x3"></span>
-                        <h4>Cras Sollicitudin</h4>
-                        <p>Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="about" class="number wow fadeInUp" data-wow-delay="300ms">
-            <!-- change the image in style.css to the class .number .container-fluid [approximately row 102] -->
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 opaline col-md-offset-6">
-                            <div class="row">
-                                <div class="col-md-offset-1 col-md-10">
-                                    <h3>Ligula Mattis Ornare Ultricies</h3>
-                                    <h5>Pellentesque Cursus Amet Parturient Etiam</h5>
-                                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                                </div>
-                            </div>
-                            <div class="row text-center">
-                                <!-- set the numbers in assets/js/scripts.js  -->
-                                <div class="col-md-4 boxes col-xs-6 col-xs-offset-3 col-lg-4 col-lg-offset-1 col-md-offset-1 col-sm-6 wow fadeInUp">
-                                    <h5>Ultricies</h5>
-                                    <h3 class="odometer 01">00000</h3>
-                                </div>
-                                <div class="col-md-4 boxes col-xs-6 col-xs-offset-3 col-lg-4 col-lg-offset-2 col-md-offset-2 col-sm-6 wow fadeInUp" data-wow-delay="300ms">
-                                    <h5>Fermentum</h5>
-                                    <h3 class="odometer 02">00000</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="news" class="blog wow fadeInUp" data-wow-delay="300ms">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7">
-                        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui. Curabitur blandit tempus porttitor. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue.</p>
-                        <a class="btn btn-danger btn-lg" href="#">Take a Look <i class="fa fa-arrow-circle-o-right"></i> </a>
-                    </div>
-                    <div class="col-md-5">
-                        <a href="#">
-                            <img src="https://unsplash.it/1200/1200?image=839" alt="" class="img-responsive">
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="history" class="story wow fadeInUp" data-wow-delay="300ms">
-            <!-- change the image in style.css to the class .story .container-fluid [approximately row 141] -->
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 opaline">
-                            <div class="row">
-                                <div class="col-md-10 col-md-offset-1">
-                                    <p class="lead"><i>Donec sed odio dui. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</i></p>
-                                    <p><i>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</i></p>
-                                    <h6 class="lead"> – Fermentum Dapibus</h6>
-                                    <p><small>Nibh Etiam Risus Bibendum<br>
-        Nullam id dolor id nibh ultricies vehicula ut id elit.</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="photos" class="gallery wow fadeInUp" data-wow-delay="300ms">
-            <div class="container">
-                <div class="row">
-                    <div class="masonry image-gallery">
-                        <div class="grid-sizer"></div>
-                        <div class="gutter-sizer"></div>
-                        <div class="item item-width2">
-                            <a href="https://unsplash.it/1000/600?image=529">
-                              <img src="https://unsplash.it/600/300?image=529" alt="" />
-                            </a>
-                        </div>
-                        <div class="item">
-                            <a href="https://unsplash.it/1000/600?image=523">
-                              <img src="https://unsplash.it/320/776?image=523" alt="" />
-                            </a>
-                        </div>
-                        <div class="item">
-                            <a href="https://unsplash.it/600/1000?image=503">
-                              <img src="https://unsplash.it/500/800?image=503" alt="" />
-                            </a>
-                        </div>
-                        <div class="item">
-                            <a href="https://unsplash.it/1000/600?image=505">
-                              <img src="https://unsplash.it/400/400?image=505" alt="" />
-                            </a>
-                        </div>
-                        <div class="item">
-                            <a href="https://unsplash.it/800/800?image=454">
-                              <img src="https://unsplash.it/4403/2476?image=454" alt="" />
-                            </a>
-                        </div>
-                        <div class="item">
-                            <a href="https://unsplash.it/800/800?image=515">
-                              <img src="https://unsplash.it/585/439?image=515" alt="" />
-                            </a>
-                        </div>
-                        <div class="item item-width2">
-                            <a href="https://unsplash.it/1000/600?image=451">
-                              <img src="https://unsplash.it/600/140?image=451" alt="" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="clients wow fadeInUp" data-wow-delay="300ms">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-01.jpg" class="img-responsive" />
-                    </div>
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-02.jpg" class="img-responsive" />
-                    </div>
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-03.png" class="img-responsive" />
-                    </div>
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-04.jpg" class="img-responsive" />
-                    </div>
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-05.jpg" class="img-responsive" />
-                    </div>
-                    <div class="col-md-2">
-                        <img src="images/logo-sample-06.png" class="img-responsive" />
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="contact" class="prefooter wow fadeInUp" data-wow-delay="300ms">
-            <!-- change the image in style.css to the class .prefooter .container-fluid [approximately row 154] -->
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <h3>Maecenas sed diam eget risus varius<br> blandit sit amet non magna.</h3>
-                            <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" placeholder="Your Email Here...">
-                                    <br>
-                                    <button type="button" class="btn btn-danger">Submit Newsletter</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h3>Your Logo</h3>
-                        <p>© 2016 Your Company. Designed and Developed by <a target="_blank" href="http://www.themeinthebox.com">ThemeintheBox.com</a></p>
-                    </div>
-                    <div class="col-md-4">
-                        <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i><i class="typcn typcn-social-twitter-circular"></i><i class="typcn typcn-social-tumbler-circular"></i><i class="typcn typcn-social-github-circular"></i><i class="typcn typcn-social-dribbble-circular"></i></p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+       
+
+    <?php
+    include("namugi.php");
+    ?>
+
         <!-- Bootstrap core JavaScript
     ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
